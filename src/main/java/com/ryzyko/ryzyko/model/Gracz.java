80666b35fbb66b11114aef1cb88cb98c9a6674cd
BibaGG <mbibam@gmail.com>
2018-01-11 22:00:52 +0100

    kolor

--- src/main/java/com/ryzyko/ryzyko/model/Gracz.java
+++ src/main/java/com/ryzyko/ryzyko/model/Gracz.java
@@ -1,5 +1,83 @@
 package com.ryzyko.ryzyko.model;
 
 public class Gracz {
-
+	private int id;
+	private String nick;
+	private int kolor;
+	private int ilePol;
+	private int ileZawodnikow;
+	private int ileKontynentow;
+	private int ilePolaczonych;
+	private int ileZabitych;
+	private int ileStraconych;
+	private int ileDodanych;
+	private int ileAtakow;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public int getKolor() {
+		return kolor;
+	}
+	public void setKolor(int kolor) {
+		this.kolor = kolor;
+	}
+	public int getIlePol() {
+		return ilePol;
+	}
+	public void setIlePol(int ilePol) {
+		this.ilePol = ilePol;
+	}
+	public int getIleZawodnikow() {
+		return ileZawodnikow;
+	}
+	public void setIleZawodnikow(int ileZawodnikow) {
+		this.ileZawodnikow = ileZawodnikow;
+	}
+	public int getIleKontynentow() {
+		return ileKontynentow;
+	}
+	public void setIleKontynentow(int ileKontynentow) {
+		this.ileKontynentow = ileKontynentow;
+	}
+	public int getIlePolaczonych() {
+		return ilePolaczonych;
+	}
+	public void setIlePolaczonych(int ilePolaczonych) {
+		this.ilePolaczonych = ilePolaczonych;
+	}
+	public int getIleZabitych() {
+		return ileZabitych;
+	}
+	public void setIleZabitych(int ileZabitych) {
+		this.ileZabitych = ileZabitych;
+	}
+	public int getIleStraconych() {
+		return ileStraconych;
+	}
+	public void setIleStraconych(int ileStraconych) {
+		this.ileStraconych = ileStraconych;
+	}
+	public int getIleDodanych() {
+		return ileDodanych;
+	}
+	public void setIleDodanych(int ileDodanych) {
+		this.ileDodanych = ileDodanych;
+	}
+	public int getIleAtakow() {
+		return ileAtakow;
+	}
+	public void setIleAtakow(int ileAtakow) {
+		this.ileAtakow = ileAtakow;
+	}
+	
+	
 }
---
+++ src/main/java/com/ryzyko/ryzyko/model/Kolor.java
@@ -0,0 +1,7 @@
+package com.ryzyko.ryzyko.model;
+
+public class Kolor {
+	public static final String[] Kolor = {
+			"Czerwony", "Niebieski", "Zielony", "Żółty", "Biały", 
+			"Czarny", "Różowy","Pomarańczowy", "Fioletowy", "Brązowy"};
+}
