6c6b67dbb0e07e9163b56cbeab5d12f3c8ec5fea
Tomasz Rytwinski <tomasz.rytwinski@capgemini.com>
2018-01-11 17:01:47 +0100

    first commit

---
+++ src/main/java/com/ryzyko/ryzyko/RyzykoApplication.java
@@ -0,0 +1,12 @@
+package com.ryzyko.ryzyko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyzykoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyzykoApplication.class, args);
+	}
+}
